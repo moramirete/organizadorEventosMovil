d59abf73693653a0129638901148b9457100d1ad
Sergio <135314849+moramirete@users.noreply.github.com>
2025-12-13 14:26:26 +0100

    Mejora en el main

--- app/src/main/java/com/example/organizadoreventosmovil/MainActivity.kt
+++ app/src/main/java/com/example/organizadoreventosmovil/MainActivity.kt
@@ -15,26 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvToggleMode = findViewById<TextView>(R.id.tvToggleMode)
+        val tvAuthPrompt = findViewById<TextView>(R.id.tvAuthPrompt)
+        val btnToggleMode = findViewById<Button>(R.id.btnToggleMode)
         val tvFormTitle = findViewById<TextView>(R.id.tvFormTitle)
         val btnAction = findViewById<Button>(R.id.btnAction)
         val groupUsername = findViewById<LinearLayout>(R.id.groupUsername)
         val groupConfirmPassword = findViewById<LinearLayout>(R.id.groupConfirmPassword)
 
-        tvToggleMode.setOnClickListener {
+        btnToggleMode.setOnClickListener {
             isLoginMode = !isLoginMode
             if (isLoginMode) {
                 // Cambiar a modo de inicio de sesión
                 tvFormTitle.text = "Iniciar Sesión"
                 btnAction.text = "Entrar"
-                tvToggleMode.text = "¿No tienes cuenta? Regístrate"
+                tvAuthPrompt.text = "¿No tienes cuenta?"
+                btnToggleMode.text = "Regístrate"
                 groupUsername.visibility = View.GONE
                 groupConfirmPassword.visibility = View.GONE
             } else {
                 // Cambiar a modo de registro
                 tvFormTitle.text = "Regístrate"
                 btnAction.text = "Registrarse"
-                tvToggleMode.text = "¿Ya tienes cuenta? Inicia Sesión"
+                tvAuthPrompt.text = "¿Ya tienes cuenta?"
+                btnToggleMode.text = "Inicia Sesión"
                 groupUsername.visibility = View.VISIBLE
                 groupConfirmPassword.visibility = View.VISIBLE
             }
